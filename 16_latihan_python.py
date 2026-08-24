9b58705b02b5d29b8bc0691a5065d9eecfa02963
Hobart <hobart.tkjsmeksa25@gmail.com>
2026-08-24 14:28:59 +0700

    Itulah Mengapa Hobart faza

---
+++ 16_latihan_python.py
@@ -0,0 +1,10 @@
+"""
+Program Menghitung Luas Persegi Panjang
+"""
+
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+
+luas = panjang * lebar
+
+print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
